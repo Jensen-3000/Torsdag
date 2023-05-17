5964f7fe44e32847fc3a26c8a6573940d43a09e8
Jensen <118268364+Jensen-3000@users.noreply.github.com>
2023-05-17 11:04:51 +0200

    add: witchcraft converter fra tal til char, til string

---
+++ Onsdag/Program.cs
@@ -0,0 +1,48 @@
+﻿// Onsdags sjov med Jan's tal systemer
+
+using Onsdag;
+
+char ch = '0';
+
+int chTal = (int)ch;
+// int strTal = (int)"7"
+
+double talD = 123.56;
+// Programmøren tager ansvaret fra compileren, ved cast
+// Mister nøjagtighed, mister decimaldel
+int talI = (int)talD;
+
+
+
+//Console.WriteLine($"Karakteren {ch} har værdien {(int)ch}");
+
+// Ascii tree
+//for (int i = 0; i < 256; i++)
+//{
+
+//    // Console.Write($"{(char)i} : {i}");
+//    Console.Write($"{i} : {(char)i} ");
+//    if (i % 16 == 0)
+//    {
+//        Console.WriteLine();
+//    }
+//}
+
+
+//int tal1 = 7230;
+//int tal2 = tal1 % 10;
+//int tal3 = tal2+0x30;
+
+
+//Console.WriteLine(tal3);
+
+
+TalConverter converter = new TalConverter();
+
+converter.SetInt(7913);
+
+Console.WriteLine(converter.GetInt());
+
+string decimalString = converter.GetDecimalString();
+
+Console.WriteLine(decimalString);
---
+++ Onsdag/TalConverter.cs
@@ -0,0 +1,35 @@
+﻿namespace Onsdag;
+
+internal class TalConverter
+{
+    int Number;
+
+    public void SetInt(int number)
+    {
+        Number = number;
+    }
+
+    public int GetInt()
+    {
+        return Number;
+    }
+
+    //int tal1 = 7230;
+    //int tal2 = tal1 % 10;
+    //int tal3 = tal2+0x30;
+
+    public string GetDecimalString()
+    {
+        int temp = Number;
+        string decimalStr = "";
+
+        while (temp > 0) // Kører indtil tallet bliver divideret/er nul
+        {
+            int rest = temp % 10; // Finder rest ved hjælp af modulus / Mindst betydne ciffer
+            char ch = (char)(rest + 0x30); // Rest laves om til char, ved hjælp af hex/ascii table
+            decimalStr = ch + decimalStr; // char bliver tilføjet til en string
+            temp /= 10; // nummeret bliver divideret med 10
+        }
+        return decimalStr;
+    }
+}
